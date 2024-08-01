b2b75a2450b7cd782b8598b5814b55796fb55182
Priyanshu14G <pgupta0628@gmail.com>
2024-08-01 17:25:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom'
+// import { About, Contacts, Experience, Feedback, Hero, Navbar, Tech, Works, } from './components';
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
